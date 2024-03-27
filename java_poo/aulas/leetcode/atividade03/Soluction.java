d24c4b78caa355218a0da9e213327faa3caec6f0
Eduardo-Silva-Rodrigues <silvarodrigueseduardo4@gmail.com>
2024-03-27 14:27:56 -0300

    Palindromo

--- java_poo/aulas/leetcode/atividade03/Soluction.java
+++ java_poo/aulas/leetcode/atividade03/Soluction.java
@@ -5,12 +5,16 @@ public class Soluction {
     // Selo de originalidade (Sem uso do chatGPT);
 
     public boolean palindromo(int num){
-        String numString = Integer.toString(num);
+        //Variável que armazena o resultado final:
         boolean valor = false;
-
-        for (int i = 0; i < numString.length(); i++){
-            if (numString.charAt(i) == numString.charAt((numString.length() - 1) - i)){
+        
+        //Loop for que compara os caracteres com começo e do final do parâmetro "num":
+        for (int i = 0; i < Integer.toString(num).length(); i++){
+            //Condicional if que verifica se os caracteres do começo e do final são iguais:
+            if (Integer.toString(num).charAt(i) == Integer.toString(num).charAt((Integer.toString(num).length() - 1) - i)){
+                //Caso sejam iguais, a variável "valor" receberá true:
                 valor = true;
+                //Senão, será retornado false:
             } else {
                 return false;
             }
