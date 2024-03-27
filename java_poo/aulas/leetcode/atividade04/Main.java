9dce9987c5c7dcf9dcb9cc096251be7686f43555
Eduardo-Silva-Rodrigues <silvarodrigueseduardo4@gmail.com>
2024-03-27 14:20:58 -0300

    Soma Repetida

--- java_poo/aulas/leetcode/atividade04/Main.java
+++ java_poo/aulas/leetcode/atividade04/Main.java
@@ -2,6 +2,6 @@ package java_poo.aulas.leetcode.atividade04;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(new Soluction().somaRepetida(45));
+        System.out.println(new Soluction().somaRepetida(38));
     }
 }
--- java_poo/aulas/leetcode/atividade04/Soluction.java
+++ java_poo/aulas/leetcode/atividade04/Soluction.java
@@ -6,30 +6,36 @@ public class Soluction {
     //Selo de originalidade (Sem uso do chatGPT);
 
     public int somaRepetida(int num){
-        String calculoEmString = "00";
-        int calculoInteger = 0;
+        //Variável que guarda o resultado:
+        int calculo = 0;
 
+        //Condicional if para verificar se o parâmetro "num" contem apenas 1 caractere:
         if (Integer.toString(num).length() == 1){
+            //Se contiver apenas 1 caractere, "num" é retornado:
             return num;
         }
         
-        while (calculoEmString.length() > 1){
-            String numeroString = Integer.toString(num);
-            char[] listaDeNumerosChar = numeroString.toCharArray();
+        //Loop while para executar o processo enquanto "calculo" conter mais de 1 caractere:
+        while (Integer.toString(calculo).length() > 1){
+            //Lista que guarda cada caractere do parâmetro "num" individualmente:
+            char[] listaDeNumerosChar = Integer.toString(num).toCharArray();
 
+            //Loop for para converter os caracteres para inteiros e realizar a soma entre eles:
             for (char numeroChar : listaDeNumerosChar){
-                calculoInteger = Integer.parseInt(Character.toString(listaDeNumerosChar[0])) + Integer.parseInt(Character.toString(numeroChar));
+                //Variável "calculo" recebe a soma entre eles:
+                calculo = Integer.parseInt(Character.toString(listaDeNumerosChar[0])) + Integer.parseInt(Character.toString(numeroChar));
             }
 
-            calculoEmString = Integer.toString(calculoInteger);
-
-            if (calculoEmString.length() == 1){
-                return calculoInteger = Integer.parseInt(calculoEmString);
+            //Condicional if verifica a quantidade de caracteres da variável "calculo":
+            if (Integer.toString(calculo).length() == 1){
+                //Se houver apenas 1 caractere na variável "calculo", então "calculo" será retornado:
+                return calculo;
+                //Se não houver apenas 1 caractere em "calculo", "num" recebe "calculo" e o processo se repete tudo de novo:
             } else {
-                num = Integer.parseInt(calculoEmString);
+                num = calculo;
             }
         }
 
-        return calculoInteger;
+        return calculo;
     }
 }
